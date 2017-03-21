eb96129e03437ee9831c3f3a75b8f033d9a7ffa0
cedric.thomas@epitech.eu <cedric.thomas@epitech.eu>
2017-03-21 23:10:50 +0100

    free header first try

---
+++ corewar/head/init_head.c
@@ -0,0 +1,13 @@
+/*
+** init_head.c for corewar in /home/cedric/delivery/CPE/CPE_2016_corewar/corewar
+** 
+** Made by 
+** Login   <cedric.thomas@epitech.eu>
+** 
+** Started on  Tue Mar 21 22:51:53 2017 
+** Last update Tue Mar 21 22:52:15 2017 
+*/
+
+#include "corewar.h"
+
+
