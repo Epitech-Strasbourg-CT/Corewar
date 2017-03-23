cbf7bfde1cefb65265b27e630f2bae7ed62ab8de
cedric.thomas@epitech.eu <cedric.thomas@epitech.eu>
2017-03-23 21:31:58 +0100

    aff fnt

---
+++ corewar/fnt/aff.c
@@ -0,0 +1,17 @@
+/*
+** VM - Corewar for aff in /home/cedric/delivery/CPE/CPE_2016_corewar/corewar/fnt
+** 
+** Made by 
+** Login   <cedric.thomas@epitech.eu>
+** 
+** Started on  Thu Mar 23 18:10:13 2017 
+** Last update Thu Mar 23 18:13:03 2017 
+*/
+
+void	aff(t_game *g, t_heads *h, t_ins *in)
+{
+  
+  
+  
+}
+  
