45890cb922cb7495b1fd303fbe2120323add96d3
cedric.thomas@epitech.eu <cedric.thomas@epitech.eu>
2017-03-23 22:15:53 +0100

    aff

--- corewar/fnt/aff.c
+++ corewar/fnt/aff.c
@@ -5,14 +5,13 @@
 ** Login   <cedric.thomas@epitech.eu>
 ** 
 ** Started on  Thu Mar 23 18:10:13 2017 
-** Last update Thu Mar 23 21:36:38 2017 
+** Last update Thu Mar 23 22:15:12 2017 
 */
+#include "my.h"
 #include "corewar.h"
 
 void	aff(t_game *g, t_heads *h, t_ins *in)
 {
-  
-  
-  
+  my_put_nbr(h->reg[in->val[0]]);
 }
   
