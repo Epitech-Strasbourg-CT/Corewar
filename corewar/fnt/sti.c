ddcb623e3e1b6a16edef6b29774ee254c7199242
Arthur Knoepflin <arthur.knoepflin@epitech.eu>
2017-03-24 13:38:02 +0100

    Merge avec modif Polo, Billy

--- corewar/fnt/sti.c
+++ corewar/fnt/sti.c
@@ -1,11 +1,11 @@
 /*
-** st.c for corewar-vm in /home/nicolaspolomack/cpe/2/CPE_2016_corewar/corewar
+** sti.c for corewar-vm in /home/nicolaspolomack/cpe/2/CPE_2016_corewar/corewar
 ** 
 ** Made by Nicolas Polomack
 ** Login   <nicolas.polomack@epitech.eu>
 ** 
 ** Started on  Thu Mar 23 17:02:39 2017 Nicolas Polomack
-** Last update Thu Mar 23 17:57:28 2017 Nicolas Polomack
+** Last update Fri Mar 24 13:33:56 2017 Arthur Knoepflin
 */
 
 #include "corewar.h"
@@ -16,14 +16,16 @@ void	sti(t_game *g, t_heads *h, t_ins *in)
   int	val;
   int	pos;
 
-  val = h->reg[in->val[0]];
+  val = h->reg[in->val[0] - 1];
+  printf("val registre : %d\n", val);
   if (in->type[1] == 1)
-    pos = h->reg[in->val[1]];
+    pos = h->reg[in->val[1] - 1];
   else
     pos = in->val[1] + ((in->type[1] == 3) ? h->pos : 0);
   if (in->type[1] == 1)
-    pos += h->reg[in->val[1]];
+    pos += h->reg[in->val[1] - 1];
   else
     pos += in->val[1] + ((in->type[1] == 3) ? h->pos : 0);
   my_strncpy(g->arena + pos, (char *)&val, REG_SIZE);
+  dump(g);
 }
